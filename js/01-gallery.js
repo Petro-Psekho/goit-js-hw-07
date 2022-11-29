ee6bd9acb5b4a1fdb2df81dff7ad33eceda4b273
Petro Psekho <psekho.pp@gmail.com>
2022-11-29 16:51:59 +0200

    solves task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -40,3 +40,47 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML(
+  "beforeend",
+  galleryItems
+    .map(
+      (galleryItem) =>
+        `<div class="gallery__item">
+        <a clss="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image"
+        src="${galleryItem.preview}" 
+        data-source="${galleryItem.original}"
+        alt= "${galleryItem.description}"></a></div>`
+    )
+    .join("")
+);
+
+galleryEl.addEventListener("click", activateImage);
+
+function activateImage(event) {
+  event.preventDefault();
+
+  console.log(event.target);
+  console.log(event.target.nodeName);
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  console.log(event.target.dataset);
+  console.log(event.target.dataset.source);
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  instance.show();
+
+  // close modal with ESC
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") instance.close();
+  });
+}
