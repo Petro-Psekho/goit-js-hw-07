1afc3696250daa0bdd3dfb9883826c3ec98c7231
Petro Psekho <psekho.pp@gmail.com>
2022-11-30 21:47:26 +0200

    solves task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -20,7 +20,37 @@
 
 // 4) Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
 
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(
+      galleryItem =>
+        `<div class="gallery__item">
+          <a class="gallery__item" href="${galleryItem.original}">
+          <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+          </a></div>`
+    )
+    .join('')
+);
+
+galleryRef.addEventListener('click', activateLightbox);
+
+function activateLightbox(e) {
+  e.preventDefault();
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+    animationSpeed: 300,
+    fadeSpeed: 300,
+    scrollZoom: false,
+  });
+}
